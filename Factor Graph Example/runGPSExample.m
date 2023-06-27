1d9aed2ed543f6e06d1abbbf628057a27de340d2
Zuxun <zuxun980916@163.com>
2023-06-27 17:59:06 +0100

    nonlinear example - GPS

---
+++ Factor Graph Example/runGPSExample.m
@@ -0,0 +1,140 @@
+function [chi2, chi2List, edges, dimX, dimZ] = runGPSExample(numberOfTimeSteps, omegaRScale, omegaQScale, testProposition4)
+    import g2o.core.*;
+    import odometry_model_answer.*;
+    
+    % Nominal odometry
+    odometry=[1 0 pi/180]';
+    
+    % GPS measurement covariance
+    R = eye(2);
+    
+    % Odometry covariance
+    Q = diag([0.1 0.05 pi/18000].^2);
+    
+    % Work out the information matrices
+    omegaR = omegaRScale * inv(R);
+    omegaQ = omegaQScale * inv(Q);
+    
+    % Ground truth array
+    trueX = zeros(3, numberOfTimeSteps);
+    
+    % Position observations
+    z = zeros(2, numberOfTimeSteps);
+    
+    % Observation matrix
+    H = [1 0 0;0 1 0];
+    
+    % First timestep
+    z(:, 1) = H * trueX(:, 1) + sqrtm(R) * randn(2, 1);
+    
+    % Now predict the subsequent steps
+    for k = 2 : numberOfTimeSteps
+        priorX = trueX(:, k-1);
+        v = sqrtm(Q) * randn(3, 1);
+        M = [cos(priorX(3)) -sin(priorX(3));
+            sin(priorX(3)) cos(priorX(3))];
+        predictedX = priorX;
+        predictedX(1:2) = predictedX(1:2) + M * (odometry(1:2) + v(1:2));
+        predictedX(3) = g2o.stuff.normalize_theta(predictedX(3) + odometry(3) + v(3));
+        trueX(:, k) = predictedX;
+        z(:, k) = H * trueX(:, k) + sqrtm(R) * randn(2, 1);
+    end
+    
+    % Create the graph
+    graph = SparseOptimizer();
+    algorithm = LevenbergMarquardtOptimizationAlgorithm();
+    graph.setAlgorithm(algorithm);
+    
+    % This array contains the set of vertices for the target state over time
+    vertices = cell(numberOfTimeSteps, 1);
+    
+    % This is the prior we compute for the graph. Unlike the true value above
+    % (which uses the real odometry), the prior here is computed using the
+    % nominal odometry information
+    X0 = zeros(3, numberOfTimeSteps);
+    
+    % Now create the vertices and edges
+    
+    for k = 1 : numberOfTimeSteps
+        
+        % Create the object state vertex
+        vertices{k} = VehicleStateVertex();
+        
+        % Set the initial estimate.
+        if (testProposition4 == false)
+            vertices{k}.setEstimate(trueX(:, k));
+        else
+            vertices{k}.setEstimate(0*trueX(:, k));
+        end
+
+        % Added the vertex to the graph.
+        graph.addVertex(vertices{k});
+        
+        % If this isn't the first vertex, predict the nominal value and add an
+        % edge
+        if (k > 1)
+            % Create the edge
+            processModelEdge = VehicleKinematicsEdge();
+            processModelEdge.setVertex(1, vertices{k-1});
+            processModelEdge.setVertex(2, vertices{k});
+            processModelEdge.setMeasurement(odometry);
+            processModelEdge.setInformation(omegaQ);
+            graph.addEdge(processModelEdge);
+            
+            % Predict the state for the initial condition
+            priorX = X0(:, k-1);
+            M = [cos(priorX(3)) -sin(priorX(3));
+                sin(priorX(3)) cos(priorX(3))];
+            predictedX = priorX;
+            predictedX(1:2) = predictedX(1:2) + M * odometry(1:2);
+            predictedX(3) = g2o.stuff.normalize_theta(predictedX(3) + odometry(3));
+            X0(:, k) = predictedX;
+        end
+                
+        % Set the initial estimate.
+        vertices{k}.setEstimate(X0(:,k));
+    
+        % Create the measurement edge
+        e = GPSMeasurementEdge();
+        
+        % Link it so that it connects to the vertex we want to estimate
+        e.setVertex(1, vertices{k});
+        
+        % Set the measurement value and the measurement covariance
+        e.setMeasurement(z(:,k));
+        e.setInformation(omegaR);
+        
+        % Add the edge to the graph
+        graph.addEdge(e);
+    end
+    
+    % Graph construction complete
+    
+    % Initialise the optimization. This is done here because it's a bit
+    % expensive and if we cache it, we can run multiple optimizations without
+    % having to build everything from scratch
+    graph.initializeOptimization();
+
+    % Optimize
+    if (testProposition4 == true)
+        graph.optimize();
+    end
+
+    % Compute the chi2 value
+    [chi2,chi2List] = graph.chi2();
+    
+    if (nargout == 5)
+        dimX = 0;
+        dimZ = 0;
+        edges = graph.edges();
+        vertices = graph.vertices();
+
+        for e = 1 : length(edges)
+            dimZ = dimZ + edges{e}.dimension();
+        end
+
+        for v =  1 : length(vertices)
+            dimX = dimX + vertices{v}.dimension();
+        end
+    end
+end
---
+++ Factor Graph Example/testGPSConsistencyMeasurement.m
@@ -0,0 +1,58 @@
+close all;
+clear all;
+clc;
+
+import g2o.core.*;
+import odometry_model_answer.*;
+
+% Number of steps per episode
+numberOfTimeSteps = 2000;
+
+% Number of episodes
+numberOfEpisodes = 100;
+
+% Omega Scales
+omegaRScale = 1;
+omegaQScale = 100;
+
+% If set to false, we test proposition 3, which initializes the graph at the
+% ground truth value, and does not optimize. If set to true, we test
+% proposition 4, which is the distribution after optimizing with noisy
+% measurements
+testProposition4 = true;
+
+chi2Store = zeros(numberOfEpisodes, 2 * numberOfTimeSteps - 1);
+chi2SumStore = zeros(numberOfEpisodes, 1);
+% edgeStore = cell(numberOfEpisodes, 1);
+
+% First run retrieves the graph dimensions
+[chi2SumStore(1), chi2Store(1, :), edges, dimX, dimZ] = ...
+    runGPSExample(numberOfTimeSteps, ...
+    omegaRScale, omegaQScale, testProposition4);
+
+% Get chi2Sum and chi2 values,
+parfor r = 2 : numberOfEpisodes
+    fprintf('%03d\n', r)
+    [chi2SumStore(r), chi2Store(r, :)] = ...
+        runGPSExample(numberOfTimeSteps, ...
+        omegaRScale, omegaQScale, testProposition4);
+end
+
+% Compute the number of degrees of freedom
+if (testProposition4 == true)
+    N = dimZ - dimX;
+else
+    N = dimZ;
+end
+
+% This figure works out and plots the mean and covarianace for the sum of
+% the chi2 values over all edges. Propositions 3 and 4 are guaranteed to
+% apply to ONLY these values.
+figure(1)
+plot(chi2SumStore)
+meanChi2 = mean(chi2SumStore);
+covChi2 = cov(chi2SumStore);
+title(sprintf('Mean: %f; Covariance %f', meanChi2, covChi2))
+
+% Compute the Consistency Measurement
+C = abs(log(meanChi2/N)) + abs(log(covChi2/(2*N)));
